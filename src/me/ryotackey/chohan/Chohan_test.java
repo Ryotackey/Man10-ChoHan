9d9a85196947f7430ff0b1b688b2954b3c7865e9
Ryotackey <ryotachey2194@gmail.com>
2017-12-15 20:22:57 +0900

    test

--- src/me/ryotackey/chohan/Chohan_test.java
+++ src/me/ryotackey/chohan/Chohan_test.java
@@ -24,7 +24,7 @@ public class Chohan_test extends JavaPlugin {
     public static double bal;
 
 
-    
+
     public VaultManager val = new VaultManager(this);
 
     @Override
---
+++ src/me/ryotackey/chohan/chohan_command.java
@@ -0,0 +1,15 @@
+package me.ryotackey.chohan;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.UUID;
+
+import static me.ryotackey.chohan.Chohan_test.*;
+
+public class chohan_command extends JavaPlugin implements CommandExecutor {
+}
